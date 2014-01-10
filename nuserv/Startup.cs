b594f35d271da7c79e92da700c331bac829968f0
romerod <daniel.romero@baseclass.ch>
2014-01-10 14:07:04 +0100

    Empty asp.net mvc solution

---
+++ nuserv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nuserv.Startup))]
+namespace nuserv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
