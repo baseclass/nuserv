817464f06f49985a7e04e7ee9ac85767e1d42406
romerod <daniel.romero@baseclass.ch>
2014-01-17 17:05:44 +0100

    Add fontawesome

--- nuserv/App_Start/BundleConfig.cs
+++ nuserv/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@
             bundles.Add(
                 new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/font-awesome.css"));
         }
 
         #endregion
