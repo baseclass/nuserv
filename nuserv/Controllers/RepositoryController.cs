dd81c3005d20556ed5311099f1367f026a406e4d
romerod <daniel.romero@baseclass.ch>
2014-01-17 16:55:03 +0100

    Added mvc RepositoryController with view and modified pages

---
+++ nuserv/Controllers/RepositoryController.cs
@@ -0,0 +1,23 @@
+﻿namespace nuserv.Controllers
+{
+    #region Usings
+
+    using System.Web.Mvc;
+
+    #endregion
+
+    public class RepositoryController : Controller
+    {
+        //
+        // GET: /Repositry/
+
+        #region Public Methods and Operators
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        #endregion
+    }
+}
