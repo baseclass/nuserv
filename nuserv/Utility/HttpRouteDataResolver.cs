f06576a15685a9a6417a950aab2980b9829e1435
romerod <daniel.romero@baseclass.ch>
2014-01-17 11:50:47 +0100

    Implement RouteData resolvement while in wcf context

--- nuserv/Utility/HttpRouteDataResolver.cs
+++ nuserv/Utility/HttpRouteDataResolver.cs
@@ -1,8 +1,15 @@
 ﻿namespace nuserv.Utility
 {
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
     using System.Net.Http;
     using System.Web;
     using System.Web.Http.Routing;
+
+    #endregion
 
     public class HttpRouteDataResolver : IHttpRouteDataResolver
     {
@@ -12,14 +19,61 @@
         {
             if (HttpContext.Current != null)
             {
-                var requestMessage = HttpContext.Current.Items["MS_HttpRequestMessage"] as HttpRequestMessage;
+                if (HttpContext.Current.Items.Contains(""))
+                {
+                    var requestMessage = HttpContext.Current.Items["MS_HttpRequestMessage"] as HttpRequestMessage;
 
-                return requestMessage.GetRouteData();
+                    if (requestMessage != null)
+                    {
+                        return requestMessage.GetRouteData();
+                    }
+                }
+                else
+                {
+                    return new RouteData(HttpContext.Current.Request.RequestContext.RouteData);
+                }
             }
 
             return null;
         }
 
         #endregion
+
+        private class RouteData : IHttpRouteData
+        {
+            #region Fields
+
+            private readonly System.Web.Routing.RouteData originalRouteData;
+
+            #endregion
+
+            #region Constructors and Destructors
+
+            public RouteData(System.Web.Routing.RouteData routeData)
+            {
+                if (routeData == null)
+                {
+                    throw new ArgumentNullException("routeData");
+                }
+                this.originalRouteData = routeData;
+                this.Route = null;
+            }
+
+            #endregion
+
+            #region Public Properties
+
+            public IHttpRoute Route { get; private set; }
+
+            public IDictionary<string, object> Values
+            {
+                get
+                {
+                    return this.originalRouteData.Values;
+                }
+            }
+
+            #endregion
+        }
     }
 }
