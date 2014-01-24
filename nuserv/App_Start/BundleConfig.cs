056995f0a10f9fa33a21c528297d6e9c095147c5
romerod <daniel.romero@baseclass.ch>
2014-01-24 09:14:00 +0100

    Remove knockout from code

--- nuserv/App_Start/BundleConfig.cs
+++ nuserv/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include("~/Scripts/knockout-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
