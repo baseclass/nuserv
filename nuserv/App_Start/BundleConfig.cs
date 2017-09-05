8ef8afafb6a83fd46b989f259757a6d71885c856
romerod <daniel.romero@baseclass.ch>
2017-09-05 11:22:48 +0200

    Switch from web nuget package to bower nuget packages

--- nuserv/App_Start/BundleConfig.cs
+++ nuserv/App_Start/BundleConfig.cs
@@ -14,26 +14,20 @@
 
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/bower_components/jquery/dist/jquery.min.js"));
 
             bundles.Add(
-                new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js")
-                    .Include("~/Scripts/angular-route.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+                new ScriptBundle("~/bundles/angular").Include("~/bower_components/angular/angular.min.js")
+                    .Include("~/bower_components/angular-route/angular-route.min.js"));
 
             bundles.Add(
-                new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+                new ScriptBundle("~/bundles/bootstrap").Include("~/bower_components/bootstrap/dist/js/bootstrap.min.js"));
 
             bundles.Add(
                 new StyleBundle("~/Content/css").Include(
-                    "~/Content/bootstrap.css",
+                    "~/bower_components/bootstrap/dist/css/bootstrap.css",
                     "~/Content/site.css",
-                    "~/Content/font-awesome.css"));
+                    "~/bower_components/font-awesome/css/font-awesome.min.css"));
         }
 
         #endregion
